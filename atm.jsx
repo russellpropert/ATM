b01b7daf6781bc8bae6af903cbc62ef0f4009ff0
Russell Propert <russellpropert@gmail.com>
2021-08-01 21:43:34 -0400

    Adding overdraw warning.

--- atm.jsx
+++ atm.jsx
@@ -1,8 +1,9 @@
-const ATMDeposit = ({onChange}) => {  
+const ATMTransaction = ({onChange, isDeposit, transactionState}) => {
+  const choice = ['Deposit', 'Withdraw'];
   return (
     <label className="label huge">
-      Deposit:&nbsp;
-      <input type="number" onChange={onChange}></input>
+      <h3> {choice[Number(!isDeposit)]}</h3>
+      <input type="number" value={transactionState} onChange={onChange} min={0}></input>
       <input type="submit" value="Submit"></input>
     </label>
   );
@@ -11,23 +12,41 @@ const ATMDeposit = ({onChange}) => {
 const Account = () => {
   const [transactionState, setTransactionState] = React.useState(0);
   const [totalState, setTotalState] = React.useState(0);
+  const [isDeposit, setIsDeposit] = React.useState(true);
+  const [overdraw, setOverdraw] = React.useState(false);
+
   let status = `Account Balance $${totalState}`;
+  let overdrawWarning = ['', 'You don\'t have that much to withdraw.']
   console.log('Account Rendered');
   
   const handleChange = event => {
-    setTransactionState(Number(event.target.value));
+    const amount = Number(event.target.value);
+    setTransactionState(
+      amount >= 0 ? amount : 0
+    );
     console.log(`handleChange ${event.target.value}`);
   };
   
   const handleSubmit = () => {
-    setTotalState(totalState + transactionState);
+    if (!isDeposit && transactionState > totalState) {
+      setOverdraw(true);
+    } else {
+      setOverdraw(false);
+      setTotalState(
+        isDeposit ? totalState + transactionState : totalState - transactionState
+      );
+    }
+    setTransactionState(0);
     event.preventDefault();
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <h2 id="total">{status}</h2>
-      <ATMDeposit onChange={handleChange}> Deposit</ATMDeposit>
+      <button onClick={() => setIsDeposit(false)}>Withdraw</button>
+      <button onClick={() => setIsDeposit(true)}>Deposit</button>
+      <ATMTransaction onChange={handleChange} isDeposit={isDeposit} setTransactionState={transactionState}></ATMTransaction>
+      <div style={{color: 'red'}}>{overdrawWarning[Number(overdraw)]}</div>
     </form>
   );
 };
